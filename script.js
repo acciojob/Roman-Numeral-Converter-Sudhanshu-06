0c77b2df8b0fcec99fd7f6ef6c0f092cdd439457
Sudhanshu Kumar <114154455+Sudhanshu-06@users.noreply.github.com>
2024-02-27 12:11:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,25 @@ function convertToRoman(num) {
     };
 
   //your code here
-
+ let result = '';
+  let currentNum = num;
+  for (let i = 0; i < 7; i++) {
+    let count = Math.floor(currentNum / obj[i][1]);
+    if (count > 0) {
+      result += obj[i][0].repeat(count);
+      currentNum %= obj[i][1];
+    }
+    if (i % 2 === 0 && i < 6 && currentNum >= obj[i + 2][1] - obj[i + 2][1] / 10) {
+      result += obj[i + 2][0] + obj[i][0];
+      currentNum -= obj[i + 2][1] - obj[i];
+    }
+    if (i % 2 === 1 && i < 5 && currentNum >= obj[i + 1][1] - obj[i + 1][1] / 10) {
+      result += obj[i + 1][0] + obj[i - 1][0];
+      currentNum -= obj[i + 1][1] - obj[i - 1][1];
+    }
+  }
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
 
 
 
