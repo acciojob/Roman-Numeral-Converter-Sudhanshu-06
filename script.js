af4c4fac6ea9c51fd3f79f55e54b90f80505a235
Sudhanshu Kumar <114154455+Sudhanshu-06@users.noreply.github.com>
2024-02-27 12:15:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
   //your code here
- let result = '';
-  let currentNum = num;
-  for (let i = 0; i < 7; i++) {
-    let count = Math.floor(currentNum / obj[i][1]);
-    if (count > 0) {
-      result += obj[i][0].repeat(count);
-      currentNum %= obj[i][1];
+	var roman = '';
+    var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    for (var i = 0; i < decimal.length; i++) {
+        while (decimal[i] <= num) {
+            roman += romanNum[i];
+            num -= decimal[i];
+        }
     }
-    if (i % 2 === 0 && i < 6 && currentNum >= obj[i + 2][1] - obj[i + 2][1] / 10) {
-      result += obj[i + 2][0] + obj[i][0];
-      currentNum -= obj[i + 2][1] - obj[i];
-    }
-    if (i % 2 === 1 && i < 5 && currentNum >= obj[i + 1][1] - obj[i + 1][1] / 10) {
-      result += obj[i + 1][0] + obj[i - 1][0];
-      currentNum -= obj[i + 1][1] - obj[i - 1][1];
-    }
-  }
-  return result;
+    return roman;
+
 }
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+console.log(convertToRoman(36));
 
 
 
